0f024cdcfe6353e9a1f78bd82ebab9a6c3c037e8
Ivana <ivana_pile@yahoo.com>
2021-02-01 17:50:34 -0500

    initial setup

---
+++ src/WeekContainer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import apiConfig from './keys';
+
+class WeekContainer extends React.Component {
+  state = {
+    fullData: [],
+    dailyData: []
+  }
+  componentDidMount = () => {
+    const weatherURL =
+      `http://api.openweathermap.org/data/2.5/forecast?zip=11001${apiConfig.owmKey}`
+
+    fetch(weatherURL)
+      .then(res => res.json())
+      .then(data => {
+        const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
+        this.setState({
+          fullData: data.list,
+          dailyData: dailyData
+        }, () => console.log(this.state))
+      })
+  }
+
+  formatDayCards = () => {
+    return this.state.dailyData.map((reading, index) => <DayCard reading={reading} key={index} />)
+  }
+
+  render() {
+    return (
+      <div>
+        {this.formatDayCards()}
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="display-1 jumbotron">5-Day Forecast.</h1>
+        <h5 className="display-5 text-muted">New York, US</h5>
+        <div className="row justify-content-center">
+
+          {this.formatDayCards()}
+
+        </div>
+      </div>
+    )
+  }
+
+  export default WeekContainer;
